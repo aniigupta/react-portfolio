6fa1b587a33df9b5a1bec530d6844ca72a122918
Aniket <Aniket8076206264@gmail.com>
2024-03-01 00:40:31 +0530

    portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div>
+        <footer className="copyright">
+            <div className="container">
+            <p>©2024<a href="/">  Aniket Gupta. </a>All rights reserved.</p>
+            </div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
